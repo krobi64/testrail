eabe62e53d01faa1befad8e04c2c5684ac0794a3
Kristine Robison <kris@tout.com>
2012-11-13 14:58:16 -0800

    cleaned up the main library file

--- lib/testrail.rb
+++ lib/testrail.rb
@@ -1,9 +1,6 @@
-require_relative 'testrail/version'
-require_relative 'testrail/config'
-require_relative 'testrail/command_helper'
-require_relative 'testrail/request'
-require_relative 'testrail/response'
-require_relative 'testrail/client'
+Dir["#{File.dirname(__FILE__)}/testrail/**/*.rb"].each do |f|
+  require f
+end
 
 module Testrail
   
