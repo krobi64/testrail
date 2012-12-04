42158a331bf3d919a6cda212a11c24e5395fa4fe
Kristine Robison <kris@tout.com>
2012-12-04 07:33:12 -0800

    moved helper to util directory and load util dir first

--- lib/testrail.rb
+++ lib/testrail.rb
@@ -1,3 +1,7 @@
+Dir[File.join(File.dirname(__FILE__), 'testrail', 'util', '*.rb')].each do |f|
+  require f
+end
+
 Dir[File.join(File.dirname(__FILE__), 'testrail', '*.rb')].each do |f|
   require f
 end
